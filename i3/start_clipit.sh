5d1f3e915d3cb91bb9c08add1876af77ff198295
Shu <shu@eample.com>
2021-11-14 15:29:32 +0300

    Add_i3_conf

---
+++ i3/start_clipit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This file was create on 01/07/2021
+killall clipit 
+sg nonet /usr/bin/clipit &
+exit 0
+
---
+++ i3/start_stardict.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This file was create on 10/03/2019
+killall stardict 
+sg nonet /usr/bin/stardict &
+exit 0
+
